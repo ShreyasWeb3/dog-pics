1e1d073fbb9c3d6e8523191f49fe197ee8f26600
ShreyasWeb3 <115789349+ShreyasWeb3@users.noreply.github.com>
2022-11-11 18:12:11 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [dogImage, setDogImage] = useState("")
+  const [breed, setBreed] = useState("random")
+
+
+  const extractData = async () => {
+    let url ;
+    if(breed === "random"){
+      url = "https://dog.ceo/api/breeds/image/random"
+
+    }else {
+      url = `https://dog.ceo/api/breed/${breed}/images/random`
+    }
+    const res = await axios.get(url)
+    setDogImage(res.data.message);
+
+  }
+    
+  const handleChange = (e) => {
+    setBreed(e.target.value);
+  }
+
+  useEffect(() => {
+    extractData();
+  },[breed])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Doggie</h1>
+    <label>Select the Dog breed &nbsp;&nbsp;</label>
+      <select name='breed' value={breed} onChange={(e) => handleChange(e)}>
+      <option value="random">Random</option>  
+      <option value="beagle">Beagle</option>
+      <option value="boxer">Boxer</option>
+      <option value="dalmatian">Dalmatian</option>
+      <option value="husky">Husky</option>
+      </select>
+      <div>
+        <img src={dogImage} />
+      </div>
+      <div>
+      <button onClick={extractData}>Next</button>
+      </div>
     </div>
   );
 }
